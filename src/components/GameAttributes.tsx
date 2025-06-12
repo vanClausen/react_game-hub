66c2657ca073ec34ace046cc08c998b0f73529cf
Sebastian Claus <claus.aka.crock@googlemail.com>
2025-06-12 14:43:41 +0200

    build game attributes

---
+++ src/components/GameAttributes.tsx
@@ -0,0 +1,42 @@
+import { List, ListItem, SimpleGrid } from "@chakra-ui/react";
+import DefinitionItem from "./DefinitionItem";
+import CriticScore from "./CriticScore";
+import { Game } from "../hooks/entities/Game";
+
+interface Props {
+  game: Game;
+}
+
+const GameAttributes = ({ game }: Props) => {
+  return (
+    <SimpleGrid columns={{ sm: 1, md: 2 }} spacing={10} marginTop={5} as="dl">
+      <DefinitionItem term="Platforms">
+        <List>
+          {game.parent_platforms.map(({ platform }) => {
+            console.log("platform:", platform);
+            return <ListItem key={platform.id}>{platform.name}</ListItem>;
+          })}
+        </List>
+      </DefinitionItem>
+      <DefinitionItem term="Metascore">
+        <CriticScore score={game.metacritic} />
+      </DefinitionItem>
+      <DefinitionItem term="Genres">
+        <List>
+          {game.genres.map((genre) => (
+            <ListItem key={genre.id}>{genre.name}</ListItem>
+          ))}
+        </List>
+      </DefinitionItem>
+      <DefinitionItem term="Publishers">
+        <List>
+          {game.publishers.map((publisher) => (
+            <ListItem key={publisher.id}>{publisher.name}</ListItem>
+          ))}
+        </List>
+      </DefinitionItem>
+    </SimpleGrid>
+  );
+};
+
+export default GameAttributes;
--- src/hooks/entities/Game.ts
+++ src/hooks/entities/Game.ts
@@ -1,4 +1,6 @@
 import { Platform } from "./Platform";
+import { Genre } from "./Genre";
+import { Publisher } from "./Publisher";
 
 export interface Game {
   id: number;
@@ -6,6 +8,8 @@ export interface Game {
   name: string;
   description: string;
   description_raw: string;
+  genres: Genre[];
+  publishers: Publisher[];
   children: string;
   background_image: string;
   parent_platforms: { platform: Platform }[];
